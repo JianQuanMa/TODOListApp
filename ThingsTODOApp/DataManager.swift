fb01d0e44ccdf830216836aae46b6fb3163bcded
Jian Ma <jianquanma@gmail.com>
2019-10-29 20:28:46 -0400

    added load and load all funcs

--- ThingsTODOApp/DataManager.swift
+++ ThingsTODOApp/DataManager.swift
@@ -21,14 +21,55 @@ public class DataManager{
     static func save<T: Encodable>(_ object: T, with fileName: String){
         let url = getDocumentDirectory().appendingPathComponent(fileName, isDirectory: false)
         do{
-            let data = try JSONEncoder.init().encode(object)
+            let data = try JSONEncoder().encode(object)
             if FileManager.default.fileExists(atPath: url.path){
                 try FileManager.default.removeItem(at: url)
             }
+            FileManager.default.createFile(atPath: url.path, contents: data, attributes: nil)
         }catch{
             fatalError("unable to save file!")
+        }
+    }
+    //load data with a format
+    static func load<T: Decodable>(with fileName: String, as fileType: T) -> T{
+        let url = getDocumentDirectory().appendingPathComponent(fileName, isDirectory: false)
+        guard !FileManager.default.fileExists(atPath: url.path) else{fatalError("File not exist!ed at \(url.path)")}
+        if let data = FileManager.default.contents(atPath: url.path){
+            do{
+                let model = try JSONDecoder().decode(T.self, from: data)
+                return model
+            }catch{
+                fatalError("could not load data")
             }
-        
+            
+        }else{
+            fatalError("data unavailable!")
+        }
+    }
+    //load data as it is
+    static func load(with fileName: String) -> Data?{
+           let url = getDocumentDirectory().appendingPathComponent(fileName, isDirectory: false)
+           guard !FileManager.default.fileExists(atPath: url.path) else{fatalError("File not exist!ed at \(url.path)")}
+           if let data = FileManager.default.contents(atPath: url.path){
+              return data
+           }else{
+               fatalError("data unavailable!")
+           }
+       }
+    
+    //load all files
+    static func loaddAll<T: Decodable>(_ type: T) -> [T]{
+        do{
+            let url = getDocumentDirectory().path
+            let allFiles = try FileManager.default.contentsOfDirectory(atPath: url)
+            var modelObjects = [T]()
+            allFiles.forEach{ file in
+                modelObjects.append(load(with: file, as: type))
+            }
+            return modelObjects
+        }catch{
+            fatalError("could not load files")
+                }
     }
     
 }
